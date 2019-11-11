4caeee0e99d986b2b573e3cdd072d4997c979d0d
Christian Blavier <cblavier@gmail.com>
2019-11-12 00:07:15 +0100

    tweaked CI

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Changix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.1.0", only: [:dev], runtime: false}
     ]
   end
 end
