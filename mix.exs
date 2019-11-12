9a349102109c4a2d187bb99a7ddd0274bfeff2eb
Christian Blavier <cblavier@gmail.com>
2019-11-12 11:06:05 +0100

    released to hex

--- mix.exs
+++ mix.exs
@@ -7,12 +7,11 @@ defmodule Changix.MixProject do
       name: "Changix",
       description: "Elixir library that gives you changelog features based on markdown files.",
       source_url: "https://github.com/cblavier/changix",
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/cblavier/changix"},
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       docs: [
         main: "Changix",
         extras: ["README.md"]
@@ -32,6 +31,14 @@ defmodule Changix.MixProject do
     [
       {:credo, "~> 1.1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/cblavier/changix"}
     ]
   end
 end
